5ff03c62ba349e413f69af674ca1ccb509bbe81b
ItsJady-Taken <davidkhun2004@gmail.com>
2024-06-25 13:22:27 +0700

    Added all nessecary files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new htmlWebpackPlugin(
+            {
+                template: './src/index.html'
+            }
+        )
+    ],
+     module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          
+          
+        ],
+      },
+}
