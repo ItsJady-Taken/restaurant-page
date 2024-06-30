5d51fc6e4ff9095aa08354d89339e77e6af073fb
ItsJady-Taken <davidkhun2004@gmail.com>
2024-06-30 14:44:21 +0700

    Project still in progress

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,8 @@ module.exports = {
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          test: /\.scss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
         },
         {
           test: /\.html$/i,
